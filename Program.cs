6e7ed1e553b3319e64abb8b69ae5244b4306dafa
Vinay <navik.vinay12@gmail.com>
2022-10-29 13:52:38 +0530

    1st commit Helloworld C# program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+//using System;
+
+namespace HelloWorldDemo222Batch
+{
+    class HelloWorld
+    {
+        static void Main (String[] args)
+        {
+        Console.WriteLine(" Println ~ Echo ~ Printing ~ Displaying messages ");
+        String str=Console.ReadLine();
+            Console.WriteLine(str);
+        }
+    }
+}
+
