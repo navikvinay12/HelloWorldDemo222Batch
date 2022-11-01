f45e8b44ea418c0ae4a083001ed738fd59bb29bb
Vinay <navik.vinay12@gmail.com>
2022-11-01 13:52:50 +0530

    Addition of 2 numbers

--- Program.cs
+++ Program.cs
@@ -1,19 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-
-
-//using System;
-
-namespace HelloWorldDemo222Batch
+﻿namespace Arithmetic
 {
-    class HelloWorld
+    class Addition
     {
-        static void Main (String[] args)
+        static void addition(int n1, int n2)
         {
-        Console.WriteLine(" Println ~ Echo ~ Printing ~ Displaying messages ");
-        String str=Console.ReadLine();
-            Console.WriteLine(str);
+            int Result = n1 + n2;
+            Console.WriteLine("So the Result after addition is : " + Result);
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition of 2 Numbers Program");
+            Console.WriteLine("Enter first integer number");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second integer number");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Addition.addition(n1, n2);
         }
     }
 }
-
