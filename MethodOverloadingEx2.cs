248858c41129101494d99a1ede0849247b0e350c
Vinay <navik.vinay12@gmail.com>
2022-11-03 21:15:26 +0530

    adding second ex of method overloading

---
+++ MethodOverloadingEx2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Animal
+{
+    pubic void animalSound()
+    {
+        System.Console.WriteLine("The animal makes a sound");
+    }
+}
+public class Pig : Animal
+{
+    pubic void animalSound()
+    {
+        System.Console.WriteLine("The pig says : wee wee");
+    }
+}
+class Dog:Animal
+{
+    pubic void animalSound()
+    {
+        System.Console.WriteLine("The dog says: bow wow");
+    }
+}
+class main
+{
+    public static void Main(String[] args)
+    {
+        Animal myAnimal = new Animal();
+        Pig myPig = new Pig();
+        Dog myDog = new myDog();
+
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
+        Console.ReadLine();
+    }
+}
