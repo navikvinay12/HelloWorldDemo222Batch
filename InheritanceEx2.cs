8a8f4b32d262c5e7817bd87742df5d7808c796a6
Vinay <navik.vinay12@gmail.com>
2022-11-03 21:06:52 +0530

    adding another example for inheritance

---
+++ InheritanceEx2.cs
@@ -0,0 +1,33 @@
+﻿
+class BaseClass
+{
+    public string name;
+    public string subject;
+    public void readers(string name,string subject)
+    {
+        this.name = name;
+        this.subject = subject;
+        Console.WriteLine("My name is :"+name);
+        Console.WriteLine("My Favourite subject is :"+subject);
+    }
+}
+class SubClass:BaseClass
+{
+    static int i = 1;
+    public SubClass()
+    {
+        Console.WriteLine(i);
+        i++;
+    }
+}
+class Sudo
+{
+    static void Main(String[] args)
+    {
+        SubClass obj=new SubClass();
+        obj.readers("Krishna","Math");
+        obj=new SubClass();
+        obj.readers("John", "C#");
+        Console.ReadLine();
+    }
+}
